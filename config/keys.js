0942b1b104ab750aea7960b37bf8c38af89b7a11
chrissouth89 <58452223+chrissouth89@users.noreply.github.com>
2020-03-14 09:19:11 -0500

    Delete keys.js

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	MongoURI:
-		"mongodb+srv://chrissouth89:Pineapplez01!@tournamenttracker-ctpow.mongodb.net/test?authSource=admin&replicaSet=TournamentTracker-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true",
-}
